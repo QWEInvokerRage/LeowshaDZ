c74061dc95f0c6d501888e6cf4e8d5e9f0b0007f
Kirill Tesla <kpobegaylo@mail.ru>
2021-11-17 00:02:51 +0300

    I study whith Leowsha

---
+++ TestVisualProject.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru-RU");
+    const int n = 4;
+    int num,i,j;
+    int Array[4][4] = {{00,01,02,03},{10,11,12,13},{20,21,22,23},{30,31,32,33}};
+    srand (time(NULL));
+    // for (i = 0; i<n; i++)
+    // {
+    //     for (j = 0;j<n; j++)
+    //     {
+    //         num =1 + rand() % 9 ; // случайные числа от 0 до 20
+    //         Array[i][j]= num;
+    //     }
+    // }
+    for (i = 0; i<n; i++)
+    {
+        for (j = 0;j<n; j++)
+        {
+            if ( i+j == n-1 )
+            {
+                cout<<" *" << Array[i][j]<<"* ";
+            }
+            else
+            {
+                cout<<"  "<< Array[i][j]<< "  ";
+            }
+        }
+        cout<< "\n";
+    }
+    // for (i = 0; i<n; i++)
+    // {
+    //     for (j = 0;j<n; j++)
+    //     {
+    //         if ( i+j == n + 1) cout<< Array[i][j];
+    //     }
+    //     cout<< "\n";
+    // }
+return 0;
+}
