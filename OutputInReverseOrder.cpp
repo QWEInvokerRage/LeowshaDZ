2b826194ec8f1245b1c3d8a7e517860337c68aa7
Kirill Tesla <kpobegaylo@mail.ru>
2021-11-17 23:52:50 +0300

    5 task

---
+++ OutputInReverseOrder.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru-RU");
+    const int n = 4;
+    int num,i,j;
+    int Array[4][4] = {{00,01,02,03},{10,11,12,13},{20,21,22,23},{30,31,32,33}};
+
+    for (i = 3; i>=0; i--)
+    {
+        for (j = 3;j>=0; j--)//0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+        {
+            cout<< " *" <<Array[i][j]<< "* ";
+        }
+        cout<< "\n";
+    }
+return 0;
+}
