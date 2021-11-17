099b6c156b57712dc88daf9bdf8dc00ca5aad057
Kirill Tesla <kpobegaylo@mail.ru>
2021-11-17 22:36:37 +0300

    4  task

---
+++ ChangeEvenElements.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru-RU");
+    const int n = 4;
+    int num,i,j;
+    int Array[4][4] = {{00,01,02,03},{10,11,12,13},{20,21,22,23},{30,31,32,33}};
+
+    for (i = 0; i<n; i++)
+    {
+        for (j = 1;j<n; j+=2)//0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+        {
+            Array[i][j+1]=Array[i][j];
+        }
+        // Function for dodick's :
+        // for (j = 0;j<n; j+=2)//0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+        // {
+        //     Array[i][j+1]=Array[i][j];
+        // }
+        // cout<< "\n";
+    }
+        for (i = 0; i<n; i++)
+    {
+        for (j = 0;j<n; j++)//0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+        {
+            cout<<" *" << Array[i][j]<<"* ";
+        }
+        cout<< "\n";
+    }
+return 0;
+}
